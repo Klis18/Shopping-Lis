7730c8c19b41ddb7dac80060249cb652eb93bc0c
Klis18 <klisnp18@gmail.com>
2025-05-02 15:46:13 -0500

    creación de estructura

---
+++ src/app/shopping/components/shopping-product-item/shopping-product-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-product-item',
+  imports: [],
+  templateUrl: './shopping-product-item.component.html',
+  styleUrl: './shopping-product-item.component.css'
+})
+export class ShoppingProductItemComponent {
+
+}
---
+++ src/app/shopping/pages/shopping-page/shopping-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-page',
+  imports: [],
+  templateUrl: './shopping-page.component.html',
+  styleUrl: './shopping-page.component.css'
+})
+export class ShoppingPageComponent {
+
+}
