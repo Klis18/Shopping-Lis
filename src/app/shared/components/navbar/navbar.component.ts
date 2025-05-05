5911901436b669e5ab8c8f0224958aa8863f5d1e
Klis18 <klisnp18@gmail.com>
2025-05-05 17:58:03 -0500

    agregado badge en icono carrito de compras

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, signal, SimpleChanges, WritableSignal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { ShoppingCarService } from '../../../shopping/services/shopping-car.service';
 
 @Component({
   selector: 'shared-navbar',
@@ -9,6 +10,19 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  
+  cantidadProductosCarrito: WritableSignal<number> = signal(0);
+
+  constructor(private shoppingCarService:ShoppingCarService){}
+
+  ngOnInit(): void {
+    this.cantidadProductosCarrito = this.shoppingCarService.getQuantityProducts();
+  }
+  
+
+
+  
 
 }
--- src/app/shopping/services/shopping-car.service.ts
+++ src/app/shopping/services/shopping-car.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, WritableSignal } from '@angular/core';
 import { ShoppingProduct } from '../interfaces/shopping-product.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,14 @@ export class ShoppingCarService {
 
   shoppingCar: ShoppingProduct[] = [];
 
+  quantityShoppingCar = signal<number>(0);
+
   constructor() { }
 
   addProductsCart(product: ShoppingProduct){
     this.shoppingCar.push(product);
+    const currentProductsCar = this.shoppingCar.length;
+    this.quantityShoppingCar.set(currentProductsCar);
     console.log('producto agregado');
   }
 
@@ -19,4 +24,12 @@ export class ShoppingCarService {
     console.log(this.shoppingCar);
     return this.shoppingCar;
   }
+
+  getQuantityProducts(){
+    return this.quantityShoppingCar;
+  }
+  // getQuantityProducts(): Observable<number>{
+  //   const cantidadProductos = this.shoppingCar.length;
+  //   return cantidadProductos;
+  // }
 }
